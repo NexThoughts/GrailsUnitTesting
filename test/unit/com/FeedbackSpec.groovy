522e0b78a0d02581cef2666e2993100de5c1ccd3
Vijay <vshukla684@gmail.com>
2015-12-23 18:16:04 +0530

    Initial Commit for unit testing with simple example

---
+++ test/unit/com/FeedbackSpec.groovy
@@ -0,0 +1,17 @@
+package com
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(Feedback)
+class FeedbackSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
---
+++ test/unit/com/UserControllerSpec.groovy
@@ -0,0 +1,17 @@
+package com
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(UserController)
+class UserControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
